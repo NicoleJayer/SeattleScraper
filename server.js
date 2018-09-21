e501c576cfb2a0def3e457d74c900890fa571f19
Nicole Richardson <njrichardson95@gmail.com>
2018-09-21 01:32:04 -0400

    mongoose database connection added to server file

--- server.js
+++ server.js
@@ -30,6 +30,24 @@ app.use(express.static(__dirname + "/public"));
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 
+//if deployed, use the deployed database. Otherwise use the local mongoHeadLines database
+var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadLines";
+
+//Connect mongoose to our database
+mongoose.connect(db, function(error){
+
+  //log any errors connecting with mongoose
+  if (error) {
+    console.log(error);
+  }
+
+//or log successfully
+  else {
+    console.log("mongoose connection is successful");
+  }
+});
+
+//port listening
 app.listen(PORT, function() {
   console.log("Listening on port: " + PORT);
 })
