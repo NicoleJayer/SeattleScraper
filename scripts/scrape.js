542ff3fca5243af679722fbb4fe76622272e851e
Nicole Richardson <njrichardson95@gmail.com>
2018-09-22 14:50:35 -0400

    made updates to scraper js file and date js file

---
+++ scripts/scrape.js
@@ -0,0 +1,31 @@
+var request = require("request");
+var cheerio = require("cheerio");
+
+var scrape = function (cb) {
+    request("http://mynorthwest.com/local/", function(err, res, body){
+
+      var $ = cheerio.load(body);
+
+      var articles = [];
+
+      $(".also").each(function(i, element){
+        var head = $(this).children("h3").text().trim();
+        var sum = $(this).children(".tease").text().trim();
+
+        if(head && sum){
+          var headNeat = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+          var sumNeat = sum.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+
+          var dataToAdd = {
+            headline: headNeat,
+            summary: sumNeat
+          };
+
+          articles.push(dataToAdd);
+        }
+      });
+      cb(artcles);
+    });
+};
+
+module.exports = scrape;
