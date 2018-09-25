b188243a71fb7dda34e93bd622997ab3ee63a646
Nicole Richardson <njrichardson95@gmail.com>
2018-09-25 14:06:28 -0400

    created json obejct in routes for scraped articles

--- config/routes.js
+++ config/routes.js
@@ -1,3 +1,15 @@
+var scrape = require("../scripts/scrape");
+
+var headlinesController = require("../controllers/headlines");
+var notesController = require("../controllers/notes");
+
+
+
+
+
+
+
+
 module.exports = function(router) {
 
     //this route renders the homepage
@@ -9,4 +21,21 @@ module.exports = function(router) {
   router.get("/saved", function(req, res) {
     res.render("saved");
   });
+
+//api route to fetch all new articles
+  router.get("/api/fetch", function(req, res) {
+    headlinesController.fetch(function(err, docs) {
+      if (!docs || docs.insertedCount === 0) {
+        res.json({
+          message: "No new articles today. Check back tomorrow!"
+        });
+      }
+      else {
+        res.json({
+          message: "Added " + docs.insertedCount + " new articles!"
+        });
+      }
+    });
+  });
+  
 }
